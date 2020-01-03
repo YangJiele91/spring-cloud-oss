ca9eb84cb4c2efcc9668959fd1d14482caeb8c75
YangJiale <353362103@qq.com>
2020-01-03 17:50:55 +0800

    配置修改

--- demo-consumer/src/main/java/com/demo/consumer/feign/client/DemoFeignClient.java
+++ demo-consumer/src/main/java/com/demo/consumer/feign/client/DemoFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="demo-service", fallback= DemoClientFallback.class)
+@FeignClient(name="demo-consumer", fallback= DemoClientFallback.class)
 public interface DemoFeignClient {
 
 	@GetMapping("/feign/getUser/{id}")
